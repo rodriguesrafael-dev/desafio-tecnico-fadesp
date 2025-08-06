fcd2fa6bf7f3d20b849046650b3bd0631f4a7857
rafaelrodrigues <rodriguesrafael.dev@gmail.com>
2025-08-06 03:15:28 -0300

    refactor: ajustada a variavel referente a string da url do mongodb

--- backend/pagamento/src/main/java/br/com/fadesp/test/pagamento/core/config/MongoDBConfig.java
+++ backend/pagamento/src/main/java/br/com/fadesp/test/pagamento/core/config/MongoDBConfig.java
@@ -1,5 +1,6 @@
 package br.com.fadesp.test.pagamento.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -9,9 +10,12 @@ import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 @Configuration
 public class MongoDBConfig {
 
+    @Value("${SPRING_DATA_MONGODB_URI:mongodb://admin:admin123@mongodb:27017/test_fadesp?authSource=admin}")
+    private String mongoUri;
+
     @Bean
     public MongoDatabaseFactory mongoConfigure(){
-        return new SimpleMongoClientDatabaseFactory("mongodb://127.0.0.1:27017/test_fadesp");
+        return new SimpleMongoClientDatabaseFactory(mongoUri);
     }
 
     @Bean
